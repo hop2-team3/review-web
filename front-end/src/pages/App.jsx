cbe61a4522670d287c0c1df4dc983b233a51fb3b
khuslen <khuslen@nest-30s-iMac.local>
2023-04-07 17:14:51 +0800

    #1_Header

--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./Home";
 import { SignUp } from "./SignUp";
+import { Categories } from "./Categories";
+import { Login } from "./Login";
+import { Forbusinesses } from "./Forbusinesses";
 
 export const App = () => {
   const router = createBrowserRouter([
@@ -12,6 +15,18 @@ export const App = () => {
     {
       path: "/signup",
       element: <SignUp />,
+    },
+    {
+      path: "/categories",
+      element: <Categories />,
+    },
+    {
+      path: "/forbusinesses",
+      element: <Forbusinesses />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
   return <RouterProvider router={router} />;
