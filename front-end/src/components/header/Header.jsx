6aa9d91114ef2c7631822b152ce406d7d1edfe9e
Flew2k <dulguunpuntsag@gmail.com>
2023-04-26 17:00:39 +0800

    example of folder structure

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg";
-import drop from "../assets/hamburger.png";
+import logo from "../../assets/logo.svg";
+import drop from "../../assets/hamburger.png";
 export function Header() {
   const [toggleDrop, setToggleDrop] = useState(false);
   const styles = {
@@ -23,7 +23,7 @@ export function Header() {
               setToggleDrop(!toggleDrop);
               console.log(toggleDrop);
             }}
-            className="sm:hidden flex"
+            className="flex sm:hidden"
             style={styles.drop}
           ></button>
           {toggleDrop ? (
@@ -32,39 +32,38 @@ export function Header() {
             Categories
           </Link>
           <hr />
-          <Link to="/Login" className="text-sm  font-normal">
+          <Link to="/Login" className="text-sm font-normal">
             Log in
           </Link>
-          <hr />
+          {/* <hr />
           <Link
             to="/Forbusinesses"
-            className="text-sm   inter  flex "
+            className="flex text-sm inter "
           >
             For businesses
           </Link>
-          <hr />
+          <hr /> */}
             </div>
           ) : (
             ""
           )}
         </div>
         <div className="w-[367px] h-[72px] sm:flex hidden justify-evenly items-center ">
-          <Link to="/categories" className="text-sm text-[#FCFBF3] font-normal">
+          {/* <Link to="/categories" className="text-sm text-[#FCFBF3] font-normal">
             Categories
-          </Link>
-          <Link to="/Login" className="text-sm text-[#FCFBF3] font-normal">
+          </Link> */}
+          <Link to="/signup" className="text-sm text-[#FCFBF3] font-normal">
             Log in
           </Link>
-          <Link
+          {/* <Link
             to="/Forbusinesses"
             className="text-sm bg-[#FCFBF3] h-[40px] w-[149.02px] bg-blue-400 rounded-[9999px] text-[#1C1C1C] inter font-bold flex justify-center items-center"
           >
 
             For businesses
-          </Link>
+          </Link> */}
         </div>
       </div>
     </div>
   );
 }
-export default Header;
