e2a5bb14b72215e4a8c3ac39f3ee7c546db8df94
khuslen <khuslen@nest-25s-iMac.local>
2023-05-17 19:02:58 +0800

    ReviewPage

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.svg";
-import drop from "../../assets/hamburger.png";
+import logo from "../assets/logo.svg";
+import drop from "../assets/hamburger.png";
 export function Header() {
   const [toggleDrop, setToggleDrop] = useState(false);
   const styles = {
@@ -16,16 +16,14 @@ export function Header() {
   return (
     <div className="bg-[#1C1C1C] h-[72px] w-full flex flex-row justify-center items-center ">
       <div className="h-[72px] w-[1360px] flex justify-between items-center px-[30px] sm:px-[80px]">
-        <Link to="/">
-          <img className="h-[34.38px] w-[139.97px] " src={logo} />
-        </Link>
+        <img className="h-[34.38px] w-[139.97px] " src={logo} />
         <div>
           <button
             onClick={() => {
               setToggleDrop(!toggleDrop);
               console.log(toggleDrop);
             }}
-            className="flex sm:hidden"
+            className="sm:hidden flex"
             style={styles.drop}
           ></button>
           {toggleDrop ? (
@@ -51,22 +49,17 @@ export function Header() {
           )}
         </div>
         <div className="w-[367px] h-[72px] sm:flex hidden justify-evenly items-center ">
-          <Link
-            to="/categoriesExplore"
-            className="text-sm text-[#FCFBF3] font-normal"
-          >
+          <Link to="/categories" className="text-sm text-[#FCFBF3] font-normal">
             Categories
           </Link>
-          <Link
-            to="/CustomerSignUp"
-            className="text-sm text-[#FCFBF3] font-normal"
-          >
+          <Link to="/Login" className="text-sm text-[#FCFBF3] font-normal">
             Log in
           </Link>
           <Link
-            to="/BusinessSignUp"
+            to="/Forbusinesses"
             className="text-sm bg-[#FCFBF3] h-[40px] w-[149.02px] bg-blue-400 rounded-[9999px] text-[#1C1C1C] inter font-bold flex justify-center items-center"
           >
+
             For businesses
           </Link>
         </div>
@@ -74,3 +67,4 @@ export function Header() {
     </div>
   );
 }
+export default Header;
