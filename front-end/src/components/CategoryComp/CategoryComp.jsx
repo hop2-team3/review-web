c5d0f079a6d2bdfe4e859736eafd4d5720339fd2
alliumiu <boloruyanga07@gmail.com>
2023-05-27 19:31:49 +0800

    home-page-fixed

--- front-end/src/components/CategoryComp/CategoryComp.jsx
+++ front-end/src/components/CategoryComp/CategoryComp.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const CategoryComp = (props) => {
-  // const logo = require(`../../assets/${props.categoryName}.png`);
-  const logo = require(`../../assets/bank.png`);
   return (
     <Link
       className="w-[290px] h-[80px] rounded-[8px] flex justify-center items-center bg-[#FFFFFF] hover:shadow-[3px_3px_2px_0px_gray] hover:scale-[1.01]"
       to={`/CategoriesExplore/${props.categoryName}`}
     >
       <div className="w-[226px] h-[32px] flex items-center gap-[20px] border-none">
-        <img className="w-[32px] h-[32px]" src={logo} alt="logo" />
         <p className="capitalize">{props.categoryName}</p>
       </div>
     </Link>
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect, useContext } from "react";
 import { CategoryComp } from "../components/CategoryComp/CategoryComp";
-// import { Link } from "react-router-dom";
 import { ReviewCarouselComp } from "../components/ReviewCarouselComp/ReviewCarouselComp";
+import background from "../assets/background.jpg";
 import { RatingStarsComp } from "../components/RatingStarsComp";
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -41,19 +41,22 @@ export const Home = () => {
   let uniqueCats = [...new Set(categories)];
 
   return (
-    <>
-      <div className="flex flex-col w-[screen] h-[auto] relative md-flex">
-        <div className=" h-[440px] w-screen flex justify-center items-center flex-row bg-[black] md-flex ">
+    <div>
+      <div className="flex flex-col w-screen h-[auto] relative md-flex overflow-hidden">
+        <div
+          className=" h-[440px] w-screen flex justify-center items-center flex-row bg-cover md-flex "
+          style={{ backgroundImage: `url(${background})` }}
+        >
           <div className=" h-[212px] w-[900px] font-[800px] text-[40px] leading-[52px] md-flex">
-            <p className="  text-white leading-[52px] text-[#FFFFFF]">
+            <p className="  text-[#146C94] leading-[52px] ">
               Read reviews. Write reviews.
             </p>
-            <p className="  text-white leading-[52px] mt-[20px] text-[#FFFFFF]">
+            <p className="  text-[#146C94] leading-[52px] mt-[20px] ">
               Find companies you can trust.
             </p>
             <div className="w-[660px] h-[64px] flex flex-row relative mt-[40px] md-flex">
               <input className="w-full h-full rounded-[32px] "></input>
-              <button className="h-[50px] w-[162px] bg-[#205CD4] rounded-[9999px] absolute right-[10px] top-[7px] text-center text-white text-[15px] font-[700]">
+              <button className="h-[50px] w-[162px] bg-[#AFD3E2] rounded-[9999px] absolute right-[10px] top-[7px] text-center text-white text-[15px] font-[700]">
                 Search
               </button>
             </div>
@@ -67,6 +70,21 @@ export const Home = () => {
                 Explore categories{" "}
               </p>
             </div>
+            <div className="mt-[2vw] flex flex-wrap h-[283px] w-[1255px] pt-[10px] place-content-evenly md-flex ">
+              {/* 2xl:h-[283px] overflow-x-auto */}
+              <CategoryComp categoryName={"restaurants"} />
+              <CategoryComp categoryName={"Coffee shop"} />
+              <CategoryComp categoryName={"Jewelry store"} />
+              <CategoryComp categoryName={"Cosmetic store"} />
+              <CategoryComp categoryName={"bank"} />
+              <CategoryComp categoryName={"Sport store"} />
+              <CategoryComp categoryName={"Shoe store"} />
+              <CategoryComp categoryName={"Men's clothing"} />
+              <CategoryComp categoryName={"Women's clothing"} />
+              <CategoryComp categoryName={"Electronic store"} />
+              <CategoryComp categoryName={"cinema"} />
+              <CategoryComp categoryName={"Car dealer"} />
+            </div>
             <div className=" flex flex-wrap  h-[283px] w-[1255px] md-flex md-[32px] pt-[10px] place-content-evenly md-flex">
               {uniqueCats?.map((el, index) => {
                 return <CategoryComp categoryName={el} key={index} />;
@@ -75,38 +93,145 @@ export const Home = () => {
           </div>
         </div>
 
-        <div className="h-[635px] w-[screen] flex flex-wrap s place-content-evenly bg-[#FCFBF3] scroll-smooth md-flex">
-          {reviews?.map((el, index) => {
-            return (
-              <ReviewCarouselComp
-                name={el.firstname}
-                company={el.lastname}
-                review={el.comment}
-                rate={el.rate}
-                key={index}
-              />
-            );
-          })}
+        <div className="h-[635px] w-[screen] flex flex-wrap place-content-evenly bg-[#FCFBF3] overflow-x-auto scroll-smooth md-flex">
+          <div className="flex flex-row">
+            <ReviewCarouselComp
+              name={"Tugu"}
+              company={"villiard"}
+              review={"Yaanaaa"}
+              rate={2}
+            />
+            <ReviewCarouselComp
+              name={"Gegee"}
+              company={"villiard"}
+              review={"Yanziin goy delguur bna. Dahin zaaval uilchluuneee.  "}
+              rate={2}
+            />
+            <ReviewCarouselComp
+              name={"Selenge"}
+              company={"villiard"}
+              review={
+                "Orchin saitai, dotor tohijilt saitai, ajilchidiin hariltsaa handlaga sain, goy uilchilgeeni gazar baina."
+              }
+              rate={3}
+            />
+            <ReviewCarouselComp
+              name={"Tsolmon"}
+              company={"Mr Lin"}
+              review={
+                "Ariun tsevriin tal deer anhaarah shaardlagatai !!!, busad talaar haritsangui gaigui, mun orchin sian tohitoi tuhtai gazar baina"
+              }
+              rate={3}
+            />
+            <ReviewCarouselComp
+              name={"Tergel"}
+              company={"Yuna"}
+              review={
+                "Orchin saitai, dotor tohijilt saitai, ajilchidiin hariltsaa handlaga sain, goy uilchilgeeni gazar baina"
+              }
+              rate={5}
+            />
+            <ReviewCarouselComp
+              name={"Sanaa"}
+              company={"Khaan"}
+              review={
+                "Uilchilgeeneii gazar baij arai udaan uilchilsen, gehdee hariltsaa handlaga iin tal deer haritsangui sain"
+              }
+              rate={3}
+            />
+            <ReviewCarouselComp
+              name={"Ujee"}
+              company={" Time Billiard"}
+              review={
+                "10 jiliin suragchdiig oroin tsagaar oruulahgui baigaa ni mash buruu, busdaar dajgui gazarshuu"
+              }
+              rate={3}
+            />
+            <ReviewCarouselComp
+              name={"Khulanaaaa"}
+              company={"Sunny"}
+              review={
+                "Hariltsaani soyol baihgui, Shiruun haritsdag, Ajilchid ni mash buruu setgelgeetei"
+              }
+              rate={1}
+            />
+          </div>
+          <div className="flex flex-row">
+            <ReviewCarouselComp
+              name={"Tsetse"}
+              company={"villiard"}
+              review={"dajgui gazar bainaaa dahin uilchluuleh bolnoo"}
+              rate={5}
+            />
+            <ReviewCarouselComp
+              name={"Balbar"}
+              company={"Ulemj Salon"}
+              review={
+                "Baingiin uilchluugch shuu, uneheer goy gazar , dahin dahin uilchluuleed baimaar. Tsaashdaa ch uilchluulseer baih bolnoo"
+              }
+              rate={5}
+            />
+            <ReviewCarouselComp
+              name={"Ujee"}
+              company={" Time Billiard"}
+              review={
+                "10 jiliin suragchdiig oroin tsagaar oruulahgui baigaa ni mash buruu, busdaar dajgui gazarshuu"
+              }
+              rate={3}
+            />
+            <ReviewCarouselComp
+              name={"Khulanaaaa"}
+              company={"Sunny"}
+              review={
+                "Hariltsaani soyol baihgui, Shiruun haritsdag, Ajilchid ni mash buruu setgelgeetei"
+              }
+              rate={1}
+            />
+            <ReviewCarouselComp
+              name={"Anand"}
+              company={"Dreamy drinks"}
+              review={
+                "Umnuh umnuhuu bodvol amt chanar mash muudsan baina. Mash udaan huleelgedeg bolson. Tsagaasaa ert haaj oroitoj ongoidog geh met"
+              }
+              rate={2}
+            />
+            <ReviewCarouselComp
+              name={"Nomio"}
+              company={" Time hospital"}
+              review={
+                "emch nar ni hariltsaa handlagiin tal deer anhaarval zohistoi"
+              }
+              rate={3}
+            />
+            <ReviewCarouselComp
+              name={"Khulanaaaa"}
+              company={"Sunnyside cafe"}
+              review={
+                "Hariltsaani soyol baihgui, Shiruun haritsdag, Ajilchid ni mash buruu setgelgeetei"
+              }
+              rate={1}
+            />
+          </div>
         </div>
-
-        <div className=" flex items-center justify-center w-[screen] h-[516px] bg-[#022A1C] md-flex">
+        <div className=" flex items-center justify-center w-[screen] h-[516px] bg-[#AFD3E2] md-flex">
           <div className="flex flex-row items-center justify-center w-[1120px] h-[516px] md-flex ">
             <div className=" flex flex-col justify-evenly w-[496px] h-[363px] md-flex">
               <p className="h-[53px] w-[175px] text-[#FCFBF3] font-[840] text-[40px] ">
                 Be heard
               </p>
               <p className="h-[68px] w-[397px] text-[#FCFBF3] leading-7 ">
-                Trustpilot is a review platform that’s open to everyone. Share
-                your experiences to help others make better choices and
-                encourage companies to up their game.
+                Views is a review platform that’s open to everyone. Share your
+                experiences to help others make better choices and encourage
+                companies to up their game.
               </p>
-              <button className="h-[56px] w-[165px] rounded-[9999px] border border-[#FCFBF3] text-[#FCFBF3] bg-[#022A1C]">
+              <button className="h-[56px] w-[165px] rounded-[9999px] border border-[#FCFBF3] text-[#FCFBF3] bg-[#AFD3E2]">
                 What we do
               </button>
             </div>
           </div>
         </div>
       </div>
-    </>
+      <div />
+    </div>
   );
 };
